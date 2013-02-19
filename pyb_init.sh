62260e663f3dfe6892a4f4423c9dc5a5e64455d6
mriehl <maximilien.riehl@gmail.com>
2013-02-19 21:32:13 +0100

    refactoring : extracted function

--- pyb_init.sh
+++ pyb_init.sh
@@ -4,18 +4,7 @@ function pyb_init(){
 
     show_pyb_init_usage
 
-    FIRST_ARGUMENT=$1
-
-    [[ ! -z "$FIRST_ARGUMENT" ]] && {
-        GIT_URL=$FIRST_ARGUMENT
-
-        git clone $GIT_URL || {
-            echo "ERROR: Could not clone git project at $GIT_URL ..maybe git is not installed, or the url is wrong?"
-            return 1
-        }
-        cd `basename $GIT_URL`
-    }
-
+    __try_to_clone_git_repository_if_argument_present_and_go_inside $@
 
     CURRENT_DIRECTORY=`pwd`
     PATH_TO_EXPECTED_BUILD_PY_FILE="$CURRENT_DIRECTORY/build.py"
@@ -46,3 +35,19 @@ function show_pyb_init_usage(){
     echo "Either initialize the current working directory or clone a git project and initialize it instead."
     return 0
 }
+
+function __try_to_clone_git_repository_if_argument_present_and_go_inside(){
+
+    FIRST_ARGUMENT=$1
+ 
+    [[ ! -z "$FIRST_ARGUMENT" ]] && {
+        GIT_URL=$FIRST_ARGUMENT
+
+        git clone $GIT_URL || {
+            echo "ERROR: Could not clone git project at $GIT_URL ..maybe git is not installed, or the url is wrong?"
+            return 1
+        }
+        cd `basename $GIT_URL`
+    }
+}
+
