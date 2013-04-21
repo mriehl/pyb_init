735e733a9a2750b040727d134abe85be00ec037d
mriehl <maximilien.riehl@gmail.com>
2013-04-21 17:54:43 +0200

    adding license headers

--- src/unittest/python/pyb_init_tests.py
+++ src/unittest/python/pyb_init_tests.py
@@ -1,3 +1,21 @@
+# coding=utf-8
+#
+# pyb_init - pybuilder project initialization
+#
+# Copyright (C) 2013 Maximilien Riehl <maximilien.riehl@gmail.com>
+#
+#        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+
 from __future__ import absolute_import
 import unittest
 from mockito import when, verify, any as any_value, unstub, mock
--- src/unittest/python/vcs_tools_tests.py
+++ src/unittest/python/vcs_tools_tests.py
@@ -1,3 +1,21 @@
+# coding=utf-8
+#
+# pyb_init - pybuilder project initialization
+#
+# Copyright (C) 2013 Maximilien Riehl <maximilien.riehl@gmail.com>
+#
+#        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#                    Version 2, December 2004
+#
+# Everyone is permitted to copy and distribute verbatim or modified
+# copies of this license document, and changing it is allowed as long
+# as the name is changed.
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+
 import unittest
 
 from pyb_init.vcs_tools import determine_project_name_from_git_url
