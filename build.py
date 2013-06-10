f5cf01bad85c81dcd9a09095972df49a52871a8b
mriehl <maximilien.riehl@gmail.com>
2013-06-10 09:46:00 +0200

    releasing 0.1.18

--- build.py
+++ build.py
@@ -35,7 +35,7 @@ license = 'WTFPL'
 name = 'pyb_init'
 url = 'https://github.com/mriehl/pyb_init'
 summary = 'pybuilder project initialization'
-version = '0.1.17'
+version = '0.1.18'
 
 default_task = ['analyze', 'publish']
 
